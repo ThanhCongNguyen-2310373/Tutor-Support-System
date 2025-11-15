6c8555bd0692844376208589efc6a3a1d00614b0
xdeadstokex <129391255+xdeadstokex@users.noreply.github.com>
2025-11-15 13:59:22 +0700

    add complete meeting api and some code decoration

--- src/meetings/meetings.controller.ts
+++ src/meetings/meetings.controller.ts
@@ -25,9 +25,12 @@ import { Role } from '@prisma/client';
 export class MeetingsController {
   constructor(private readonly meetingsService: MeetingsService) {}
 
-  /**
-   * UC_STU_01: Student đặt lịch hẹn
-   */
+
+
+
+//######################################
+//## UC_STU_01: Student đặt lịch hẹn ###
+//######################################
   @Post('book')
   @Roles(Role.STUDENT)
   @ApiOperation({ summary: 'Student đặt lịch hẹn với tutor' })
@@ -38,9 +41,12 @@ export class MeetingsController {
     return this.meetingsService.createBooking(req.user.id, dto);
   }
 
-  /**
-   * UC_STU_05: Student đánh giá buổi học
-   */
+
+
+
+//###############################################
+//## UC_STU_05: Student đánh giá buổi học api ###
+//###############################################
   @Post(':id/rating')
   @Roles(Role.STUDENT)
   @ApiOperation({ summary: 'Student đánh giá buổi học đã hoàn thành' })
@@ -56,9 +62,12 @@ export class MeetingsController {
     return this.meetingsService.submitRating(req.user.id, id, dto);
   }
 
-  /**
-   * Get my meetings (Student or Tutor)
-   */
+
+
+
+//#############################################
+//## Get my meetings (Student or Tutor) api ###
+//#############################################
   @Get('my-meetings')
   @Roles(Role.STUDENT, Role.TUTOR)
   @ApiOperation({ summary: 'Xem danh sách meetings của mình' })
@@ -67,9 +76,12 @@ export class MeetingsController {
     return this.meetingsService.getMyMeetings(req.user.id, req.user.role);
   }
 
-  /**
-   * Get meeting detail
-   */
+
+
+
+//#############################
+//## Get meeting detail api ###
+//#############################
   @Get(':id')
   @ApiOperation({ summary: 'Xem chi tiết meeting' })
   @ApiResponse({ status: 200, description: 'Lấy chi tiết thành công' })
@@ -79,9 +91,12 @@ export class MeetingsController {
     return this.meetingsService.getMeetingById(id, req.user.id, req.user.role);
   }
 
-  /**
-   * Cancel meeting
-   */
+
+
+
+//#########################
+//## Cancel meeting api ###
+//#########################
   @Patch(':id/cancel')
   @Roles(Role.STUDENT, Role.TUTOR)
   @ApiOperation({ summary: 'Hủy meeting' })
@@ -92,4 +107,25 @@ export class MeetingsController {
   async cancelMeeting(@Request() req, @Param('id', ParseIntPipe) id: number) {
     return this.meetingsService.cancelMeeting(req.user.id, req.user.role, id);
   }
+
+
+
+
+//###########################
+//## Complete meeting api ###
+//###########################
+  @Patch(':id/complete')
+  @Roles(Role.STUDENT, Role.TUTOR)
+  @ApiOperation({ summary: 'Complete meeting' })
+  @ApiResponse({ status: 200, description: 'Complete meeting thành công' })
+  @ApiResponse({ status: 400, description: 'Không thể Complete meeting đã Cancel hoặc Pending' })
+  @ApiResponse({ status: 403, description: 'Không có quyền Complete meeting này' })
+  @ApiResponse({ status: 404, description: 'Meeting không tồn tại' })
+  async completeMeeting(@Request() req, @Param('id', ParseIntPipe) id: number){
+    return this.meetingsService.completeMeeting(req.user.id, req.user.role, id);
+  }
+
+
+
+
 }
