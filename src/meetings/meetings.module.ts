8289bef77d2be9e66e21bfa78ddd01e76c97a84a
ThanhCongNguyen-2310373 <your-email@example.com>
2025-11-19 12:59:27 +0700

    Add WebSocket_Notifications, Update_Usecase and Documentation for it

--- src/meetings/meetings.module.ts
+++ src/meetings/meetings.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { MeetingsController } from './meetings.controller';
 import { MeetingsService } from './meetings.service';
 import { EmailModule } from '../email/email.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, NotificationsModule],
   controllers: [MeetingsController],
   providers: [MeetingsService],
   exports: [MeetingsService],
